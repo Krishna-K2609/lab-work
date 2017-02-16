71fcadbde26ffc2acdd6841050717688e811b628
Krishna-K2609 <krisha2609@g.ucla.edu>
2017-02-15 16:37:20 -0800

    adding corrected script

--- exercise-6/ebird_summarizer.sh
+++ exercise-6/ebird_summarizer.sh
@@ -5,6 +5,7 @@
 new_liner.sh eBird.csv
 
 # the next line will replace all extra commas and will replace the contents of formatted_eBird.csv
-
 sed 's/,\s/ /g' formatted_eBird.csv > reformatted_eBird.csv
+
+# the next line will use the reformatted_eBird.csv to execute function 'does_something.py'
 python does_something.py reformatted_eBird.csv
